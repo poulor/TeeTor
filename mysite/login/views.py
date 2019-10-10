615d04338533a2656f0ba89df012648808607e58
Rory Eiffe <eifferblue1@gmail.com>
2019-10-09 21:18:04 -0400

    Wrote some views and urls for the login functionality. I just need to work on transferring the data from the form to the database.

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import User
+
+#An introductory message:
+def index(request):
+    template = loader.get_template('login/index.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+#this will prompt the user to input their information.
+def detail(request, question_id):
+    return HttpResponse("Please enter username and password." % question_id)
+
+#This greets the user.
+def results(request, question_id):
+    response = "Hi! %s."
+    return HttpResponse(response % question_id)
+
+def submit(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    q = User(username = username, password = password)
+    q = User(username = "Uh oh", password = "stinky")
+    q.save()
+    return HttpResponse("Submitted response")
