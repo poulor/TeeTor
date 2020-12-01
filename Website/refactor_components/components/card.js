099e2eff002466bc1aba6987b7064555aba69752
Rory Eiffe <eifferblue1@gmail.com>
2020-12-01 02:03:40 -0500

    Combined mentor card and mentee card into one component: card. When instantiating, need to pass in a 'type' prop that indicates whether it is a 'mentee' or 'mentor' card.

--- Website/refactor_components/components/card.js
+++ Website/refactor_components/components/card.js
@@ -3,8 +3,18 @@ import styles from "./css/card.module.css";
 import FullBulb from "./resources/fullBulb.png";
 import HalfBulb from "./resources/halfBulb.png";
 
-// Use case: 
-/* <Mentor 
+/* 
+  Use Case:
+<Card 
+type = "mentee"
+name = "Myles Carr" 
+title = "Hamster" 
+score = {451} 
+subjects = {["HTML","CSS", "JS", "MEAN", "Python"]} 
+url = "https://thumbs.gfycat.com/PleasedOrdinaryDeinonychus-max-1mb.gif"/> */
+
+/* <Card 
+type = "mentor"
 name = "Hughie Campbell" 
 title = "Sup' Killer" 
 bio = "The best character in the Boys" 
@@ -29,12 +39,19 @@ function findBulbs (rating) {
     return bulbs;
 }
 
-export default class menteeCard extends Component {
+export default class Card extends Component {
     state = {
+        type: "",
         bulbs: [],
     }
-
     componentDidMount = () => {
+        // When mounted, check if this is a mentor or mentee card.
+        if (this.props.type == "mentor") this.setState({type:"mentor"});
+        else this.setState({type:"mentee"});
+        // Initialize bulbs, based on rating. Each elements
+        // in bulbs is a separate image (either full or half)
+        // lightbulb, that displays the user's rating out of 5. 
+        // ex: a rating of 3.5 would be 3 full light bulbs and one half lightbulb
         let bulbs = findBulbs(this.props.rating);
         let comps = [];
         for(let i = 0; i < bulbs.full; i ++){
@@ -45,7 +62,6 @@ export default class menteeCard extends Component {
         }
         this.setState({bulbs: comps});
     }
-    
   render() {
     return (
       <div className={[styles.card, this.props.className].join(" ")}>
@@ -62,18 +78,14 @@ export default class menteeCard extends Component {
           />
         </div>
 
+        {/* display content relevant to mentees */}
+        {this.state.type == "mentee" && 
         <div className={[styles.profileCardBody].join(" ")}>
           <h className={styles.profileCardName}>{this.props.name}</h>
           <div className={styles.profileCardTitle}>{this.props.title}</div>
 
-          <div className={styles.profileCardBio}>
-            <span>{this.props.bio}</span>
-          </div>
-
-          <div className = {styles.bulbRating}>
-          {this.state.bulbs.map((bulb) => (
-              <div className = {styles.bulb}>{bulb}</div>
-            ))}
+          <div className={styles.bulbRating}>
+            <span>{this.props.score}xp</span>
           </div>
 
           <div className={[styles.tagSubjects].join(" ")}>
@@ -81,8 +93,29 @@ export default class menteeCard extends Component {
               <div className = {styles.sub}>{subject}</div>
             ))}
           </div>
-          
-        </div>
+        </div>}
+        {/* display content relevant to mentors */}
+        {this.state.type == "mentor" && 
+        <div className={[styles.profileCardBody].join(" ")}>
+            <h className={styles.profileCardName}>{this.props.name}</h>
+            <div className={styles.profileCardTitle}>{this.props.title}</div>
+
+            <div className={styles.profileCardBio}>
+            <span>{this.props.bio}</span>
+            </div>
+
+            <div className = {styles.bulbRating}>
+            {this.state.bulbs.map((bulb) => (
+                <div className = {styles.bulb}>{bulb}</div>
+            ))}
+            </div>
+
+            <div className={[styles.tagSubjects].join(" ")}>
+            {this.props.subjects.map((subject) => (
+                <div className = {styles.sub}>{subject}</div>
+            ))}
+            </div>
+        </div>}
       </div>
     );
   }
