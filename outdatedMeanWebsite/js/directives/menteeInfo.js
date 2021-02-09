79546e977fe3fe38a1aa6d65375d7a6ef695eed7
aSehat <47791263+aSehat@users.noreply.github.com>
2021-02-09 15:55:08 -0500

    Deleted and old/outdated content

--- outdatedMeanWebsite/js/directives/menteeInfo.js
+++
@@ -1,9 +0,0 @@
-app.directive('menteeInfo', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            info: '='
-        },
-        templateUrl: '../js/directives/menteeInfo.html'
-    };
-});
