b68987176e6678791ad7c816187a045663b9d86e
Richard <rjpoulose@gmail.com>
2021-02-16 17:58:00 -0500

    setup initial file for Alert.js

---
+++ Website/client/src/components/alert/alertCard.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import styles from "./css/card.module.css";
+
+// props to take success, warning, failure
+export default class alertCard extends Component {
+
+}
