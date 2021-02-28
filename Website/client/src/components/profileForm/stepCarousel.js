da500501f07af093492eb3cd5749be34e4f06621
roryeiffe <eiffer@rpi.edu>
2021-02-28 18:39:50 -0500

    Created visuals for step component using an MIT licensed step component as a reference

--- Website/client/src/components/profileForm/stepCarousel.js
+++ Website/client/src/components/profileForm/stepCarousel.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import styles from "./style/stepCarousel.module.css";
+import { Link, withRouter } from 'react-router-dom';
 
 export default class stepCarousel extends Component {
+  // state stores the steps. Each step includes a link and a name
   state = {
     steps: [],
   };
   componentDidMount = () => {
     let steps = this.props.steps;
     // Add a number attribute to each step (1,2,3,etc.)
-    for (let i = 0;i < steps.length; i ++){
-        steps[i].number = i + 1;
+    for (let i = 0; i < steps.length; i++) {
+      steps[i].number = i + 1;
     }
     // Update state with props
     this.setState({
@@ -19,15 +21,15 @@ export default class stepCarousel extends Component {
 
   render() {
     return (
-      <div className={styles.body}>
-        {this.state.steps.map((step) => (
-          <div key={step.link}>
-            <div className = {styles.circle}>
-                {step.number}
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className = {styles.container}>
+        <ul className = {styles.progressBar}>
+          {this.state.steps.map((step) => (
+              <li>
+                <p className = {styles.step}>{step.step}</p>
+              </li>
+            ))}
+        </ul>
+         </div>
     );
   }
 }
