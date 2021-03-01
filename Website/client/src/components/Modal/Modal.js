e83f648a0308a0fa0a856549d2f896edc26c8108
Covenant <cfaluyi@gmail.com>
2021-02-28 20:46:46 -0500

    Added dynamic component to Modal

--- Website/client/src/components/Modal/Modal.js
+++ Website/client/src/components/Modal/Modal.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import "./modal.css";
 
-const Modal = ({show, children}) => {
-    return show ? <div>{children}</div> : null;
+const Modal = ({show, onClose, children}) => {
+    return show ? <div class="modal" id="modal">
+    <h2>Modal Window</h2>
+    <div class="content">{children}</div>
+    <div class="actions">
+      <button class="toggle-button" onClick={onClose}>
+        close
+      </button>
+    </div>
+  </div> : null;
 }
 
 
