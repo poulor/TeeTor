6f9513e7d038f13f08221c542ef00488af70eaff
Richard <rjpoulose@gmail.com>
2021-03-08 23:02:17 -0500

    Added .css file and initial setup for alert

--- Website/client/src/actions/alert.js
+++ Website/client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // This function gets passed to a specific use case. In this case, it is register.js. That use case passes in specific values
-// relevent to the function. In this case, msg and alertType for the view. Timeout = 4000 sets a default timeout
+// relevant to the function. In this case, msg and alertType for the view. Timeout = 4000 sets a default timeout
 export const setAlert = (msg, alertType, timeout = 4000) => (dispatch) => {
   //Generate a random id for the alert
   const id = uuidv4();
@@ -13,6 +13,6 @@ export const setAlert = (msg, alertType, timeout = 4000) => (dispatch) => {
     payload: { msg, alertType, id },
   });
 
-  // Trigers the 'remove_alert' in the reducer causing it to get removed from the state after 4 seconds
+  // Triggers the 'remove_alert' in the reducer causing it to get removed from the state after 4 seconds
   setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
