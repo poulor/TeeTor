620e732819166183c0647c80c87ddba95b736406
roryeiffe <eiffer@rpi.edu>
2021-03-12 19:49:00 -0500

    Updated Step Carousel Component so that we can pass in components as a prop and render them by clicking on the corresponding step.

--- Website/client/src/components/test.js
+++ Website/client/src/components/test.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Carousel from "./profileForm/stepCarousel";
+import Test1 from "./test1";
+import Test2 from "./test2";
 
 export default class test extends Component {
   render() {
     return(
     <div>
       <h1>This is the testing page</h1>
-      <Carousel steps = {[{link : "link1", step : "Profile"}, {link : "link2", step : "School Terms"},{link : "link3", step : "Course Schedule"}, {link: "link4", step: "Unavailability"} ]}></Carousel>
+      <Carousel steps = {[{step : "Profile", component:<Test1/>}, {step : "School Terms", component:<Test2/>},{step : "Course Schedule", component:<Test1/>}, {step: "Unavailability", component:<Test2/>} ]}></Carousel>
     </div>);
   }
 }
---
+++ Website/client/src/components/test2.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class test2 extends Component {
+  render() {
+    return(
+    <div>
+      <h1>Test 2</h1>
+    </div>);
+  }
+}
