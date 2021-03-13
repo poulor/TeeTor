c01221c90bff1974010a4d03ef872c601818fb7a
roryeiffe <eiffer@rpi.edu>
2021-03-12 19:49:00 -0500

    Updated Step Carousel Component so that we can pass in components as a prop and render them by clicking on the corresponding step.

--- Website/client/src/components/profileForm/stepCarousel.js
+++ Website/client/src/components/profileForm/stepCarousel.js
@@ -1,35 +1,63 @@
+import { ContextHandlerImpl } from "express-validator/src/chain";
 import React, { Component } from "react";
 import styles from "./style/stepCarousel.module.css";
-import { Link, withRouter } from 'react-router-dom';
 
+// The way this component works is we pass in a list of objects. 
+// Each object contains a name (passed in as 'step') and content
+// that we want to display when we click on that step (passed in as
+// 'component'). 
+// Use case (With 2 steps):
+{/* <Carousel steps = {[{step : "Profile", component:<Test1/>}, {step : "School Terms", component:<Test2/>}]}></Carousel> */}
+// The component renders the steps by mapping the state to a 
+// carousel display and updating the state when a new step is clicked
+// so that that step can be displayed:
 export default class stepCarousel extends Component {
   // state stores the steps. Each step includes a link and a name
   state = {
     steps: [],
+    currentStep: 1,
+    currentComponent: null,
   };
   componentDidMount = () => {
     let steps = this.props.steps;
     // Add a number attribute to each step (1,2,3,etc.)
+    // This will be used as a key as well as allow us to 
+    // display the number in the circle
     for (let i = 0; i < steps.length; i++) {
-      steps[i].number = i + 1;
+      steps[i].key = i + 1;
     }
     // Update state with props
     this.setState({
       steps: steps,
     });
+    // update state with the current component that we want to display:
+    this.setState({
+      currentComponent: steps[this.state.currentStep - 1].component,
+    });
   };
 
+  // when we click on a new step, we want to update which content is being
+  // displayed:
+  updateStep = (stepNum) => {
+    // update state value of which step we are showing:
+    this.setState({ currentStep:stepNum });
+    // updates the component attribute in state:
+    this.setState({
+      currentComponent: this.state.steps[stepNum - 1].component,
+    });
+  };
   render() {
     return (
-      <div className = {styles.container}>
-        <ul className = {styles.progressBar}>
+      <div className={styles.container}>
+        <ul className={styles.progressBar}>
           {this.state.steps.map((step) => (
-              <li>
-                <p className = {styles.step}>{step.step}</p>
-              </li>
-            ))}
+            <li onClick={this.updateStep.bind(this,step.key)}>
+              <p className={styles.step}>{step.step}</p>
+            </li>
+          ))}
         </ul>
-         </div>
+        <div>{this.state.currentComponent}</div>
+      </div>
     );
   }
 }
---
+++ Website/client/src/components/test1.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class test1 extends Component {
+  render() {
+    return(
+    <div>
+      <h1>Test 1</h1>
+    </div>);
+  }
+}
