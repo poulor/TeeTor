c49cad28cd3af70c9ea6b93bcc8fc72ae7657b82
Covenant <cfaluyi@gmail.com>
2021-03-17 21:35:28 -0400

    Modal changes

--- Website/client/src/components/Modal/Modal.js
+++ Website/client/src/components/Modal/Modal.js
@@ -2,15 +2,18 @@ import React from 'react';
 import "./modal.scss";
 
 const Modal = ({show, onClose, children}) => {
-    return show ? <div class="modal" id="modal">
-    <h2>Modal Window</h2>
-    <div class="content">{children}</div>
-    <div class="actions">
-      <button class="toggle-button" onClick={onClose}>
-        Close
-      </button>
-    </div>
-  </div> : null;
+    return show ? 
+    <div class="background">
+      <div class="modal" id="modal">
+        <h2>Modal Window</h2>
+        <div class="content">{children}</div>
+        <div class="actions">
+          <button class="toggle-button" onClick={onClose}>
+          Close
+        </button>
+        </div>
+      </div>
+    </div> : null;
 }
 
 
