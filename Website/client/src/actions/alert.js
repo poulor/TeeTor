9c053643530dc28b57964bd15b39f0fb94314148
Richard <rjpoulose@gmail.com>
2021-03-21 19:25:42 -0400

    Finished alert notification - added prop type, position, timeout and styling

--- Website/client/src/actions/alert.js
+++ Website/client/src/actions/alert.js
@@ -2,15 +2,19 @@ import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // This function gets passed to a specific use case. In this case, it is register.js. That use case passes in specific values
-// relevant to the function. In this case, msg and alertType for the view. Timeout = 4000 sets a default timeout
-export const setAlert = (msg, alertType, timeout = 30000) => (dispatch) => {
+// relevant to the function. In this case, msg, alertType and position for the view.
+// position: 1 = top right, 2 = top right, 3 = bottom left, 4 = bottom right, 5 = center
+// position = 2 sets a default position
+// timeout = 4000 sets a default timeout of 4 seconds
+export const setAlert = (msg, alertType, position = 'top-right', timeout = 4000) => (dispatch) => {
   //Generate a random id for the alert
   const id = uuidv4();
   // Triggers the reducer because its "DISPATCHING" something of type set_alert, reducer js file will know what to do with it
   dispatch({
     //Dispatch the alert with the passed in msg and type
     type: SET_ALERT,
-    payload: { msg, alertType, id },
+    // payload: { msg, alertType, id },
+    payload: { msg, alertType, position, timeout, id },
   });
 
   // Triggers the 'remove_alert' in the reducer causing it to get removed from the state after 4 seconds
