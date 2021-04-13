6df3ef936a05d5916d46307dd47d61cdfe8a056e
Covenant <cfaluyi@gmail.com>
2021-04-13 19:34:48 -0400

    AutoTag styling

--- Website/client/src/components/layout/autoTag/AutoTag.js
+++ Website/client/src/components/layout/autoTag/AutoTag.js
@@ -1,17 +1,29 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import ReactTags from 'react-tag-autocomplete'
-
+import './style/autoTag.css'
 
 export const AutoTag = ({fileName}) => {
     // const fileroot = '../../../utils/';
     // const filedir = fileroot.concat(fileName);
     // const dataset = require(filedir)
-    const dataset = require('../../../utils/subjects.json')
+    const subjects = require('../../../utils/subjects.json')
+    const languages = require('../../../utils/languages.json')
+    var dataset = [];
+    switch(fileName) {
+      case 'subjects':
+        dataset = subjects;
+        break;
+      case 'languages':
+        dataset = languages;
+        break;
+      default:
+        dataset = [...languages, ...subjects]
+    }
 
-    const [suggestions, setSuggestions] = useState([dataset])
+    const [suggestions] = useState(dataset)
     const [tags, setTags] = useState([])
     const reactRef = React.createRef()
-
+    console.log(suggestions)
     const onDelete = (i)  => {
         let newTags = tags.slice(0)
         newTags.splice(i, 1)
